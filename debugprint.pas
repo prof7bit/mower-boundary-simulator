e034a45cc19b21b559911e880c3b70e90978fc79
Bernd Kreuss <b.kreuss@xecro.com>
2022-07-05 13:39:12 +0200

    add line number tp debug print outpt (if debug symbols are present)

--- debugprint.pas
+++ debugprint.pas
@@ -4,23 +4,60 @@ unit DebugPrint;
 
 interface
 
-uses
-  Classes, SysUtils;
-
 procedure Print(Txt: String);
 procedure Print(Int: Integer);
 procedure Print(Dbl: Double);
 procedure Print(Fmt: String; Args: array of const);
 
 implementation
+uses
+  Classes, SysUtils, LazTracer;
+
 const
   FILENAME = 'debug.txt';
 
 var
-  F: TFileStream;
+  F: TFileStream = nil;
+  LineNumbers: Boolean = True;
+
+function GetCallingLine: String;
+var
+  BP, OldBP: Pointer;
+  Info: String;
+  SL: TStringArray;
+  Filename, LineNumber: String;
+begin
+  Result := '';
+  BP := get_caller_frame(get_frame);
+  while Assigned(BP) do begin
+    Info := GetLineInfo(get_caller_addr(BP), True);
+    if Pos('line', Info) = 0 then begin
+      LineNumbers := False;
+      exit;
+    end;
+    if Pos('debugprint.pas', Info) = 0 then begin
+      SL := Info.Split(' ');
+      Filename := SL[Length(SL) - 1];
+      LineNumber := SL[Length(SL) - 3];
+      Exit(Filename + ':' + LineNumber);
+    end;
+    OldBP := BP;
+    BP := get_caller_frame(BP);
+    if (BP <= OldBP) or (BP > (StackBottom + StackLength)) then
+      BP := Nil;
+  end;
+end;
 
 procedure Print(Txt: String);
+var
+  LN: String;
 begin
+  if LineNumbers then begin
+    LN := GetCallingLine;
+    if LN <> '' then begin
+      Txt := '[' + LN + '] ' + Txt;
+    end;;
+  end;
   {$ifdef windows}
   if not Assigned(F) then
     F := TFileStream.Create(FILENAME, fmCreate);
